d727d5eab6da3dede732f4feffdf1d64bfb8184c
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-11-04 09:56:50 -0400

    feat: update in views and controllers

--- views/template/app.php
+++ views/template/app.php
@@ -1,10 +1,3 @@
-<?php
-require 'dados.php';
-$id = $_REQUEST['id'];
-$filtrado = array_filter($livros, fn($l) => $l['id'] == $id);
-$livro = array_pop($filtrado);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,20 +23,7 @@ $livro = array_pop($filtrado);
   </header>
 
   <main class="mx-auto max-w-screen-lg space-y-6">
-    <?= $livro['titulo'] ?>
-    <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-      <div class="flex">
-        <div class="w-1/3">imagem</div>
-        <div class="space-y-1">
-          <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
-          <div class="text-xs italic"><?= $livro['autor'] ?></div>
-          <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliação)</div>
-        </div>
-      </div>
-      <div class="text-sm mt-2">
-        <?= $livro['descricao'] ?>
-      </div>
-    </div>
+    <?php require "views/{$view}.view.php"; ?>
   </main>
 </body>
 
