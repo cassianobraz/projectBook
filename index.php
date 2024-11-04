64b79296e4dce0c27c361c88dc7d88ccfd72255a
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-11-04 09:08:38 -0400

    feat: update in components and structure the project

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require 'dados.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,22 +34,24 @@
         placeholder="Pesquisar..." />
       <button type="submit">🔎</button>
     </form>
-
-    <section class="space-y-4">
+    <!-- Lista de livros -->
+    <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <!-- Livro -->
-      <div class="w-1/3 p-2 rounded border-stone-800 border-2 bg-stone-900">
-        <div class="flex">
-          <div class="w-1/3">imagem</div>
-          <div>
-            <div class="font-semibold">titulo</div>
-            <div class="text-xs italic">Autor</div>
-            <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliação)</div>
+      <?php foreach ($livros as $livro): ?>
+        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+          <div class="flex">
+            <div class="w-1/3">imagem</div>
+            <div class="space-y-1">
+              <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+              <div class="text-xs italic"><?= $livro['autor'] ?></div>
+              <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliação)</div>
+            </div>
+          </div>
+          <div class="text-sm mt-2">
+            <?= $livro['descricao'] ?>
           </div>
         </div>
-        <div class="text-sm">
-          Descrição
-        </div>
-      </div>
+      <?php endforeach; ?>
     </section>
 
   </main>
