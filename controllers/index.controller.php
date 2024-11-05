3ff385273a7e5e051c91bd9d4e0cff05650a8c75
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-11-05 13:53:49 -0400

    feat: update in dados from database connect

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -1,4 +1,6 @@
 <?php
-require 'dados.php';
+
+$db = new DB();
+$livros = $db->livros();
 
 view('index', ['livros' => $livros]);
--- controllers/livro.controller.php
+++ controllers/livro.controller.php
@@ -7,4 +7,4 @@ $id = $_REQUEST['id'];
 $filtrado = array_filter($livros, fn($l) => $l['id'] == $id);
 $livro = array_pop($filtrado);
 
-view('livro', ['livro' => $livro]);
+view('livro', compact('livro'));
