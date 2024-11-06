82cc5fac1798fd0cdee3702e1c0244851973eab1
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-11-06 14:01:58 -0400

    feat: update in call

--- database.php
+++ database.php
@@ -1,12 +1,30 @@
 <?php
 
+class Livro
+{
+  public $id;
+  public $titulo;
+  public $autor;
+  public $descricao;
+}
 class DB
 {
   public function livros()
   {
     $db = new PDO('sqlite:database.sqlite');
     $query = $db->query("select * from livros");
-    return $query->fetchAll();
+    $items = $query->fetchAll();
+    $retorno = [];
+
+    foreach ($items as $item) {
+      $livro = new Livro();
+      $livro->id = $item['id'];
+      $livro->titulo = $item['titulo'];
+      $livro->autor = $item['autor'];
+      $livro->descricao = $item['descricao'];
+      $retorno[] = $livro;
+    }
+
+    return $retorno;
   }
 }
-
